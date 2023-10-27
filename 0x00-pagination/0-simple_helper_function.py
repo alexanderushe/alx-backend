5da586af80afb645b7d166266693a5d4296fe32e
alexanderushe <alex.f.ushe@gmail.com>
2023-10-27 14:01:32 +0200

    pycode pass

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""Pagination function to calculate indices.
-takes two arguments page & page_size and returns tuple
-"""
+
 from typing import Tuple
 
-def index_range(page, page_size) -> Tuple[int, int]:
-    """Pagination function to calculate indices. 
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+
+    """Pagination function to calculate indices.
     takes two arguments page & page_size and returns tuple
     """
-
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
-
