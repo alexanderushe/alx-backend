dac5142bec270a4dfb1fe3ddeffbf84c40b70115
Alex Ushe <alex.f.ushe@gmail.com>
2023-10-27 03:56:53 +0200

    calculate indices of a page
    calculate start and end indexes of a page

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Pagination function to calculate indices"""
+from typing import Tuple
+def index_range(page, page_size) -> Tuple[int,int]:
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
