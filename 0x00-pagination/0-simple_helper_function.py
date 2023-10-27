2e0582bd0e217ca71f354678796b2a03c4a55589
alexanderushe <alex.f.ushe@gmail.com>
2023-10-27 14:05:25 +0200

    helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,12 +2,12 @@
 
 from typing import Tuple
 
+"""Pagination function to calculate indices.
+takes two arguments page & page_size and returns tuple
+"""
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-
-    """Pagination function to calculate indices.
-    takes two arguments page & page_size and returns tuple
-    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
