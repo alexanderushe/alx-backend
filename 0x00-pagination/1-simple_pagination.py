df501ee187e4b28212a6bdb9e76355333c281fae
Alex Ushe <alex.f.ushe@gmail.com>
2023-10-27 04:26:15 +0200

    pagination
    simple paginaiton

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""Pagination"""
+
+import csv
+from typing import Tuple, List
+
+def index_range(page, page_size) -> Tuple[int,int]:
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return[]
+        return data[start:end]
+
+
