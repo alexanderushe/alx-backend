646e071575e0cf9efab7b2af6c8fa5082d5faac3
alexanderushe <alex.f.ushe@gmail.com>
2023-10-27 14:41:36 +0200

    simple paagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,12 +8,19 @@ from typing import Tuple, List
 
 
 def index_range(page, page_size) -> Tuple[int, int]:
+    """ calculates the start and end indicies of a given page and page size
+    args:
+        page: the page number
+        page_size: number of items per page
+    returns: a tuple
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
 
 
 class Server:
+
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
@@ -22,7 +29,8 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """Cached dataset of the baby name data
+        returns a list of lists
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -33,6 +41,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ retrieves a page of baby name data
+            args: page - page number (1-indexed)
+            returns:
+            list of lists where each sublist represents a baby named record
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
